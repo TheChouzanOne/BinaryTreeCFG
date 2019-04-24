237497eae95af32e7c8656dd0080338a165b2ed9
rakuthesenpai <raul.r.h.rosas@gmail.com>
2019-04-23 19:12:51 -0700

    Minor fixes

--- tree.py
+++ tree.py
@@ -3,12 +3,20 @@ import BinaryTree as bt
 import subprocess
 
 try:
-    checkerInput = input("Ingrese la cadena: ")
-    checkerOutput = subprocess.Popen(["./checker"], stdout=subprocess.PIPE, stdin=subprocess.PIPE,
-                                     shell=True, universal_newlines=True)
-    checkerOutput.stdin.write(str(checkerInput))
-    checkerOutput = checkerOutput.communicate()[0].strip()
-    print(bt.createNode(checkerInput))
-    print(checkerOutput)
+    while (True):
+        checkerInput = input("Ingrese la cadena: ")
+        if(checkerInput == ''):
+            print('Invalid Output')
+            continue
+        checkerOutput = subprocess.Popen(["./checker"], stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                                         shell=True, universal_newlines=True)
+        checkerOutput.stdin.write(str(checkerInput))
+        checkerOutput = checkerOutput.communicate()[0].strip()
+        if(checkerOutput == 'Nothing'):
+            print('Invalid Output')
+        else:
+            print(bt.createNode(checkerInput))
+            print(checkerOutput)
+
 except KeyboardInterrupt:
     pass
